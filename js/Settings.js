9be8c348b765f50e981930ca483ad40eb95238ea
unknown <szymkiewiczmateusz1@gmail.com>
2022-11-26 19:27:46 +0100

    Settings - Screen Size

--- js/Settings.js
+++ js/Settings.js
@@ -26,12 +26,17 @@ class Settings{
         let this2 = this;
         this.element = document.createElement("div");
         this.element.classList.add("settings");
-       this.element.innerHTML = `<h2>Ustawienia</h2>`;
+       this.element.innerHTML = `<h2>Ustawienia</h2><h3>Rozmiar okna</h3><label>min</label><input type="range" list="dl" value="25" max="40" min="10"><label>max</label><datalist id="dl"><option>0</option><option>10</option><option>20</option><option>30</option><option>40</option></datalist>`;
         document.querySelector(".game-container").appendChild(this.element);
        utils.wait(200);
        this.esc = new KeyPressListener("Escape", () => {
             this2.close();
         });
+       document.querySelector(".settings > input[type=range]").addEventListener("change", function(){
+           let skala = document.querySelector(".settings > input[type=range]").value/10;
+           console.log(skala)
+           document.querySelector(".game-container").style.transform = `scale(${skala}) translateY(39%)`;
+       })
    }
 
 }
