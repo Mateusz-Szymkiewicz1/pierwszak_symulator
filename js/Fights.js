b20c08f1ab5339efe1f947dd22bf19bb6e539b76
User100 <szymkiewiczmateusz1@gmail.com>
2024-02-22 22:27:29 +0100

    fights ui

--- js/Fights.js
+++ js/Fights.js
@@ -10,7 +10,18 @@ class Fights{
      this.onComplete();
  }  
  show_fights(){
-    
+    window.fights.forEach(el => {
+        let div = document.createElement('div');
+        div.className = 'fight';
+        div.innerHTML = `
+            <h3>${el.name}</h3>
+            <img src="images/characters/${el.name.toLowerCase()}.png">
+            <span>${el.reward}<img src="images/Objects/gold.png"></span>
+            <p>${el.desc}</p>
+            <button>Walcz</button>
+        `;
+        document.querySelector('.fights').appendChild(div);
+    })
  } 
 async init() {
      document.querySelector("canvas").style.filter = "blur(4px)";
@@ -22,6 +33,16 @@ async init() {
      document.querySelector(".game-container").appendChild(this.element);
      this.element.setAttribute("tabindex", "0")
      this.element.focus();
+
+    window.fights = [
+        {
+            name: "Manekin",
+            reward: 0,
+            difficulty: 0,
+            desc: "Dosłownie manekin."
+        }
+    ]
+
      this.show_fights();
      utils.wait(200);
      this.esc = new KeyPressListener("Escape", () => {
